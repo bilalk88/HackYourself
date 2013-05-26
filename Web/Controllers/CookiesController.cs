4acdf242291cd061156094fcdc0571b9c921c4cb
Troy Hunt <troyhunt@hotmail.com>
2013-05-26 10:36:19 +1000

    Added a page to receive cookies via query string

---
+++ Web/Controllers/CookiesController.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+  public class CookiesController : Controller
+  {
+    public ActionResult Index(string c)
+    {
+      ViewBag.CookieString = c;
+
+      var cookieCollection = new HttpCookieCollection();
+
+      if (!string.IsNullOrEmpty(c))
+      {
+        foreach (var cookie in c.Split(';').Select(pair => pair.Split('=')))
+        {
+          cookieCollection.Add(new HttpCookie(cookie[0], cookie.Length > 1 ? cookie[1] : string.Empty));
+        }
+      }
+
+      return View(cookieCollection);
+    }
+  }
+}
