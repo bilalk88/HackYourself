0b673d8840a7125b09c1d7939d82584a65484386
Troy Hunt <troyhunt@hotmail.com>
2013-06-10 15:06:33 +1000

    Added a data collection template for form data

---
+++ Web/Controllers/DataCollectionController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+  public class DataCollectionController : Controller
+  {
+    [HttpPost]
+    public ActionResult Index(FormCollection formCollection)
+    {
+      return View(formCollection);
+    }
+  }
+}
