5cd5e0e9f4d03dad52d363c807a9aec8d5a20e13
Troy Hunt <troyhunt@hotmail.com>
2013-06-23 14:50:39 +1000

    Little script to harvest credentials when form embedded in an HTTP page

---
+++ Web/Controllers/CountdownController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+  public class CountdownController : Controller
+  {
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Index(string ContentPlaceHolderDefault_BodyContent_CustomerLogin_1_txtUsername, string ContentPlaceHolderDefault_BodyContent_CustomerLogin_1_txtPassword)
+    {
+      return Redirect("https://www.countdown.co.nz/onecard/panels/customer-login");
+    }
+  }
+}
