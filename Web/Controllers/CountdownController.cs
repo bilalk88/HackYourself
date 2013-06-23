2ba3dfc76e1a0e8b625b11df82b2f34dbd944cc7
Troy Hunt <troyhunt@hotmail.com>
2013-06-24 06:39:07 +1000

    Disabled the form.

--- Web/Controllers/CountdownController.cs
+++ Web/Controllers/CountdownController.cs
@@ -6,12 +6,16 @@ namespace Web.Controllers
   {
     public ActionResult Index()
     {
+      return RedirectToAction("Index", "Home");
+
       return View();
     }
 
     [HttpPost]
     public ActionResult Index(string ContentPlaceHolderDefault_BodyContent_CustomerLogin_1_txtUsername, string ContentPlaceHolderDefault_BodyContent_CustomerLogin_1_txtPassword)
     {
+      return RedirectToAction("Index", "Home");
+
       Response.Status = "307 Temporary Redirect";
       Response.AddHeader("Location", "https://www.countdown.co.nz/onecard/panels/customer-login");
       return View("Empty");
